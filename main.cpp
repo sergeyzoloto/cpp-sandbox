d291a3be1c490a7809606f410640f7548f41ea00
sergeyzoloto <sergiozoloto@gmail.com>
2023-07-20 15:04:53 +0200

    init commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// A statement is a basic unit of computation in a C++ program
+// Every C++ program is a collection of statements organized in a certain way
+
+// Define function before you can use it
+int addNumbers(int first_param, int second_param)
+{
+  int result = first_param + second_param;
+  return result;
+}
+
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+
+  return 0;
+}
