59c7d8f069af17d841e4820de88027ea8d71cd12
sergeyzoloto <sergiozoloto@gmail.com>
2023-07-20 17:00:19 +0200

    auto variables

--- main.cpp
+++ main.cpp
@@ -19,6 +19,37 @@ int getName()
   std::getline(std::cin, full_name);
 
   std::cout << "Hello " << full_name << "!" << std::endl;
+
+  return 0;
+}
+
+// C++ core language vs Standard library vs STL
+// Standard library is an extension with reusable components
+// STL is a collection of container types
+
+int autoVariables()
+{
+  auto var1{12};
+  auto var2{13.0};
+  auto var3{14.0f};
+  auto var4{15.0l};
+  auto var5{'e'};
+
+  // int modifier suffixes
+  auto var6{123u};  // unsigned
+  auto var7{123ul}; // unsigned long
+  auto var8{123ll}; // long long
+
+  std::cout << "var1 occupies : " << sizeof(var1) << " bytes" << std::endl;
+  std::cout << "var2 occupies : " << sizeof(var2) << " bytes" << std::endl;
+  std::cout << "var3 occupies : " << sizeof(var3) << " bytes" << std::endl;
+  std::cout << "var4 occupies : " << sizeof(var4) << " bytes" << std::endl;
+  std::cout << "var5 occupies : " << sizeof(var5) << " bytes" << std::endl;
+  std::cout << "var6 occupies : " << sizeof(var6) << " bytes" << std::endl;
+  std::cout << "var7 occupies : " << sizeof(var7) << " bytes" << std::endl;
+  std::cout << "var8 occupies : " << sizeof(var8) << " bytes" << std::endl;
+
+  return 0;
 }
 
 int main()
@@ -30,7 +61,8 @@ int main()
 
   int sum = first_number + second_number;
   std::cout << "sum : " << sum << std::endl;
-  getName();
+  // getName();
+  autoVariables();
 
   return 0;
 }
