de9ab86258d08c08507af34ec5fc68676de924a5
sergeyzoloto <sergiozoloto@gmail.com>
2023-07-22 14:24:29 +0200

    order of constructor destructor calls

--- classes/destructors/main.cpp
+++ classes/destructors/main.cpp
@@ -27,7 +27,7 @@ Dog::Dog(std::string_view name_param, std::string_view breed_param, int age_para
 Dog::~Dog() // ! ~ (tilde) destructor
 {
   delete p_age;
-  std::cout << "Dog destructor called for : " << name << std::endl;
+  std::cout << "Dog destructor called for " << name << std::endl;
 }
 
 void create_and_delete_dog()
@@ -40,7 +40,24 @@ void create_and_delete_dog()
 int main()
 {
 
-  create_and_delete_dog();
+  Dog dog1("Doggy1", "Shepherd", 2);
+  Dog dog2("Doggy2", "Shepherd", 3);
+  Dog dog3("Doggy3", "Shepherd", 5);
+  Dog dog4("Doggy4", "Shepherd", 1);
+
+  /*
+  Result:
+
+  Dog constructor called for Doggy1
+  Dog constructor called for Doggy2
+  Dog constructor called for Doggy3
+  Dog constructor called for Doggy4
+  Done!
+  Dog destructor called for Doggy4
+  Dog destructor called for Doggy3
+  Dog destructor called for Doggy2
+  Dog destructor called for Doggy1
+  */
 
   std::cout << "Done!" << std::endl;
   return 0;
