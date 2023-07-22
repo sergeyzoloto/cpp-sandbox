635abb93ea88c17bf6dbede8f01f0102b120b113
sergeyzoloto <sergiozoloto@gmail.com>
2023-07-22 13:57:58 +0200

    managing class objects t/ pointers

--- classes/main.cpp
+++ classes/main.cpp
@@ -10,5 +10,18 @@ int main()
   cylinder1.set_base_radius(2.0);
   std::cout << "base_radius : " << cylinder1.get_base_radius() << std::endl;
 
+  // Managing a stack object through pointers
+  Cylinder *p_cylinder1 = &cylinder1;
+  // ! -> Arrow pointer
+  std::cout << "volume (p_cylinder1): " << p_cylinder1->volume() << std::endl;
+
+  // Create a heap object through the new operator
+  Cylinder *p_cylinder2 = new Cylinder(100, 2); // in Heap
+
+  std::cout << "volume (p_cylinder2): " << p_cylinder2->volume() << std::endl;
+
+  delete p_cylinder2;
+  p_cylinder2 = nullptr;
+
   return 0;
 }
